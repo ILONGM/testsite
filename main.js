2b9c212a8dca653a917144095049e0a563fb4583
ILONGM <62313777+ILONGM@users.noreply.github.com>
2024-11-22 21:53:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 //import './style.css'
 const link = document.createElement('link');
 link.rel='stylesheet';
-link href = './style.css';
+link.href = './style.css';
 document.head.appendChild(link);
 
 const stocks = [
